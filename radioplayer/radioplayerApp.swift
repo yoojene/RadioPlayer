235d558a73db97aa9beadd8bf94faf35b101884a
Eugene Cross <eugene.cross@gmail.com>
2023-08-26 14:54:31 +0100

    Initial Commit

---
+++ radioplayer/radioplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  radioplayerApp.swift
+//  radioplayer
+//
+//  Created by Eugene on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct radioplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
